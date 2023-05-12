98fea0de70ab5505c14c044f9a8311d21539c30c
David F <dom7745@gmail.com>
2023-05-12 21:27:48 +0200

    a csoport társam dolga lett volna

--- templates/pages/allmessages.tpl.php
+++ templates/pages/allmessages.tpl.php
@@ -1,1 +1,56 @@
-toDo
+<?php
+    try{
+        $dbh->query('SET NAMES utf8 COLLATE utf8_hungarian_ci');
+
+        $sqlSelect = "SELECT 
+                        msgs.messageId, 
+                        msgs.userName, 
+                        msgs.message, 
+                        msgs.eventDateTime,
+                        users.felhasznalo,
+                        users.id,
+                        users.csaladi_nev,
+                        users.uto_nev 
+                      FROM messages msgs
+                      LEFT JOIN felhasznalok users ON msgs.userName = users.felhasznalo
+                      ORDER BY msgs.eventDateTime DESC";
+        $sth = $dbh->prepare($sqlSelect);
+        $sth->execute();
+        
+
+        if(!$count = $sth->rowCount()){
+            ?>
+                Nincs megtekinthető üzenet az adatbázisban. <a href="?oldal=kapcsolat">Üzenet küldéshez kattintson ide!</a>
+            <?php
+        } else{
+            echo('<div class="row"><div class="col">');
+            echo('Összesen' . $sth->rowCount() . ' darab üzenet.<br><br>');
+            echo('<table class="table_msgs table table-hover">');
+            echo('<thead><tr><th scope="col">#</th><th scope="col" class="col-dateTime">Dátum</th><th scope="col" class="theMessage">Üzenet</th><th scope="col" class="col-sender">küldő</th></tr></thead>');
+            echo('<tbody>');
+            $counter = 1;
+            while( $row = $sth->fetch(PDO::FETCH_ASSOC) ){
+                if($row['id'] == null){
+                     $sender = $row['userName'] . '<br><span class="sender_desc">(nem regisztrált)</span>';
+                } else {
+                    $sender = $row['csaladi_nev'] . ' ' . $row['uto_nev'] . '<br><span class="sender_desc"> -userName: ' . $row['felhasznalo'] . '<br> - id: ' . $row['id'] . '</span>';
+                }
+
+                if($row['messageId'] % 2 == 0){
+                    echo('<tr class="evenRow_msg"><th scope="row">' . $counter . '</th><td class="col-dateTime">' . $row['eventDateTime'] . '</td><td class="theMessage"><span id="msg_' . $counter . '">' . $row['message'] . '</span></td><td class="col-sender">' . $sender . '</td></tr>');
+                } else {
+                    echo('<tr class="oddRow_msg"><th scope="row">' . $counter . '</th><td class="col-dateTime">' . $row['eventDateTime'] . '</td><td class="theMessage"><span id="msg_' . $counter . '">' . $row['message'] . '</span></td><td class="col-sender">' . $sender . '</td></tr>');
+                }
+
+                $counter++;
+            }
+            echo('</tbody>');
+            echo('</table>');
+            echo('</div></div>');
+            
+        }
+    }catch (PDOException $e){
+        $errorMessage = 'Hiba: ' .$e->getMessage();
+        echo($errorMessage);
+    }
+?>
--- templates/pages/galeria.tpl.php
+++ templates/pages/galeria.tpl.php
@@ -1,1 +1,82 @@
-to do
+<?php
+$kepek = array();
+$olvaso = opendir($MAPPA);
+while (($fajl = readdir($olvaso)) !== false)
+    if (is_file($MAPPA.$fajl)) {
+        $vege = strtolower(substr($fajl, strlen($fajl)-4));
+        if (in_array($vege, $TIPUSOK))
+            $kepek[$fajl] = filemtime($MAPPA.$fajl);            
+    }
+closedir($olvaso);
+
+// Űrlap ellenőrzés:
+if (isset($_POST['kuld'])) {
+    //print_r($_FILES);
+    foreach($_FILES as $fajl) {
+        if ($fajl['error'] == 4);   // Nem töltött fel fájlt
+        elseif (!in_array($fajl['type'], $MEDIATIPUSOK))
+            $uzenet[] = " Nem megfelelő típus: " . $fajl['name'];
+        elseif ($fajl['error'] == 1   // A fájl túllépi a php.ini -ben megadott maximális méretet
+                    or $fajl['error'] == 2   // A fájl túllépi a HTML űrlapban megadott maximális méretet
+                    or $fajl['size'] > $MAXMERET) 
+            $uzenet[] = " Túl nagy állomány: " . $fajl['name'];
+        else {
+            $vegsohely = $MAPPA.strtolower($fajl['name']);
+            if (file_exists($vegsohely))
+                $uzenet[] = " Már létezik: " . $fajl['name'];
+            else {
+                move_uploaded_file($fajl['tmp_name'], $vegsohely);
+                $uzenet[] = ' Ok: ' . $fajl['name'];
+                header("Refresh:0");
+            }
+        }
+    }        
+}
+
+
+
+?>
+<h1>Feltöltés a galériába:</h1>
+<?php
+    if (!empty($uzenet))
+    {
+        echo '<ul>';
+        foreach($uzenet as $u)
+            echo "<li>$u</li>";
+        echo '</ul>';
+    }
+?>
+    <form action="" method="post"
+                enctype="multipart/form-data">
+        <label>Első:
+            <input type="file" name="elso" required>
+        </label>
+        <label>Második:
+            <input type="file" name="masodik">
+        </label>
+        <label>Harmadik:
+            <input type="file" name="harmadik">
+        </label>        
+        <input type="submit" name="kuld">
+      </form> 
+
+
+<div id="galeria">
+    <h1>Galéria</h1>
+    <?php
+    arsort($kepek);
+    foreach($kepek as $fajl => $datum)
+    {
+    ?>
+        <div class="kep">
+            <a href="<?php echo $MAPPA.$fajl ?>">
+                <img src="<?php echo $MAPPA.$fajl ?>">
+            </a>            
+            <p>Név:  <?php echo $fajl; ?></p>
+            <p>Dátum:  <?php echo date($DATUMFORMA, $datum); ?></p>
+        </div>
+    <?php
+    }
+    ?>
+</div>
+
